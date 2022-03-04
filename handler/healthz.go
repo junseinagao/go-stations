0a1134891aad654fc39a3e5aa983691f0e37a033
junseinagao <junsei.nagao@gmail.com>
2022-03-04 23:53:30 +0900

    [statuion5] json.NewEncoder() で実装し直した

--- handler/healthz.go
+++ handler/healthz.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/TechBowl-japan/go-stations/model"
@@ -18,11 +19,13 @@ func NewHealthzHandler() *HealthzHandler {
 // ServeHTTP implements http.Handler interface.
 func (h *HealthzHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	response := model.HealthzResponse{
-		Message:"string",
+		Message:"OK",
 	}
-	jsonBytes,_ := json.Marshal(response)
+	
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write(jsonBytes)
-	// json.NewEncoder(w).Encode(response)
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		log.Println(err)
+	}
 }
