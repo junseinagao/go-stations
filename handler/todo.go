6004baa639da00cb7db9783393b38025142945bb
junseinagao <junsei.nagao@gmail.com>
2022-07-01 18:09:24 +0900

    手動のリクエストでは正しいレスポンスが返ってくるのに、テストでは404が返ってくる

--- handler/todo.go
+++ handler/todo.go
@@ -24,35 +24,69 @@ func NewTODOHandler(svc *service.TODOService) *TODOHandler {
 
 func (h *TODOHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
-	// * HTTP Method が POST 以外だったら早期return
-	if r.Method != http.MethodPost {
-		return
-	}
+	switch r.Method {
+	case http.MethodPost:
+		// * request body を decode する
+		var requestBody model.CreateTODORequest
+		if err := json.NewDecoder(r.Body).Decode(&requestBody); err != nil {
+			log.Println(err)
+			return
+		}
+		// * subject が 空文字の場合は 早期return
+		if requestBody.Subject == "" {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
 
-	// * request body を decode する
-	var requestBody model.CreateTODORequest
-	if err := json.NewDecoder(r.Body).Decode(&requestBody); err != nil {
-		log.Println(err)
-		return
-	}
-	// * subject が 空文字の場合は 早期return
-	if requestBody.Subject == "" {
+		// * DB に TODO を保存
+		response, err := h.Create(r.Context(), &requestBody)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		// * HTTP Responseを返す
 		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusBadRequest)
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(response)
+		return
+
+	case http.MethodPut:
+		// * request body を decode する
+		var requestBody model.UpdateTODORequest
+		if err := json.NewDecoder(r.Body).Decode(&requestBody); err != nil {
+			log.Println(err)
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		// * ID が 空文字の場合 または Subject が 空文字の場合は 早期return
+		if requestBody.Subject == "" || requestBody.ID == 0 {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		// * DB の TODO を更新
+		response, err := h.Update(r.Context(), &requestBody)
+		if err != nil {
+			log.Println(err)
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		// * HTTP Responseを返す
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(response)
+		return
+
+	default:
 		return
 	}
-
-	// * DB に TODO を保存
-	response, err := h.Create(r.Context(), &requestBody)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	// * HTTP Responseを返す
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(response)
 }
 
 // Create handles the endpoint that creates the TODO.
@@ -71,8 +105,10 @@ func (h *TODOHandler) Read(ctx context.Context, req *model.ReadTODORequest) (*mo
 
 // Update handles the endpoint that updates the TODO.
 func (h *TODOHandler) Update(ctx context.Context, req *model.UpdateTODORequest) (*model.UpdateTODOResponse, error) {
-	_, _ = h.svc.UpdateTODO(ctx, 0, "", "")
-	return &model.UpdateTODOResponse{}, nil
+	todo, err := h.svc.UpdateTODO(ctx, req.ID, req.Subject, req.Description)
+	return &model.UpdateTODOResponse{
+		TODO: *todo,
+	}, err
 }
 
 // Delete handles the endpoint that deletes the TODOs.
